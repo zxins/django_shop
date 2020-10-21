a698ab51e6827ed7874c0e5f22c007c676abecb1
zhuxin <624042309@qq.com>
2020-10-21 18:49:19 +0800

    extra_app & goods models

--- django_shop/settings.py
+++ django_shop/settings.py
@@ -41,6 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'DjangoUeditor',
+    'users',
+    'goods',
+    'trade',
+    'user_operation'
 ]
 
 MIDDLEWARE = [
