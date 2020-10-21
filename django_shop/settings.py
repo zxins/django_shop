e06fe3eefda4713e05bce6569851c630edc062f2
zhuxin <624042309@qq.com>
2020-10-21 17:09:33 +0800

    settings

--- django_shop/settings.py
+++ django_shop/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'NAME': 'django_shop',
+        'USER': 'root',
+        'PASSWORD': os.environ.get('MYSQL_PASS'),
+        'OPTIONS': {'init_command': 'SET default_storage_engine=INNODB;'}
     }
 }
 
